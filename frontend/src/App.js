25cd90b5101fcfd033de42f53160d88d95f8e82a
AEDavik <davik.andreas@outlook.com>
2023-11-01 11:38:58 +0000

    adds start page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Log from "./pages/log/log";
+import Home from "./pages/home/home"
+import Log from "./pages/log/log"
 import Scan from "./pages/scan/scan"
 
 
@@ -8,7 +9,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"/>
+        <Route path="/" element={<Home/>}/>
         <Route path="/log" element={<Log/>}/>
         <Route path="/scan" element={<Scan/>}/>
       </Routes>
---
+++ frontend/src/pages/home/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+    return(
+    <article>
+        <h1>This is the first page shown after login</h1>
+    </article>
+    )
+
+
+
+}
