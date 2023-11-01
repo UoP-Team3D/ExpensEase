6074ac2319eb49d692f4ff62f5233b0f401be950
AEDavik <davik.andreas@outlook.com>
2023-11-01 11:53:24 +0000

    Adds non-refresh links

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 export default function Home() {
     return(
     <article>
+        <Link to="/scan">Scan</Link>
         <h1>This is the first page shown after login</h1>
     </article>
     )
