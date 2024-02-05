0812e25e01974d8311f289da8506600987f0551d
Erdit <llerdit@gmail.com>
2024-02-05 15:13:20 +0000

    SCRUM-20 example complete

--- ExpensEase/src/run.py
+++ ExpensEase/src/run.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+# run.py 
+
 app = Flask(__name__)
 CORS(app)  # cors means cross-origin resource sharing, it allows us to make requests from the frontend to the backend even if they are on different domains or ports
            # it is a security feature that is enabled by default in most browsers, but we need to enable it in the backend as well
