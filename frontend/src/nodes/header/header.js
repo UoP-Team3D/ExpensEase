7803812c09948933eecd37c81368b3f5066c4a09
georgealderr <georgec.alder@outlook.com>
2024-02-13 14:48:59 +0000

    design on navbar

--- frontend/src/nodes/header/header.js
+++ frontend/src/nodes/header/header.js
@@ -5,11 +5,13 @@ import './style.css';
 const Header = () =>{
     return(
         <header>
+            <div class='navbar'>
             <ul>
-                <li><NavLink to="/budget-managing">Manager</NavLink></li>
-                <li><NavLink to="/scan">scan</NavLink></li>
-                <li><NavLink to="/history">History</NavLink></li>
+                <li><NavLink to="/budget-managing"activeClassName='active'>Manager</NavLink></li>
+                <li><NavLink to="/scan"activeClassName='active'>scan</NavLink></li>
+                <li><NavLink to="/history"activeClassName='active'>History</NavLink></li>
             </ul>
+            </div>
         </header>
     )
 
