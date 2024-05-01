fe7125e881fd6bcd095cd742e6e0679b0af5e5df
3rdit <llerdit@gmail.com>
2024-05-01 14:39:07 +0100

    fix: fixed import

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, make_
+from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
 from routes.auth import auth_blueprint
 from routes.receipt import receipt_blueprint
