eb89736df698c3751a05283ef507d9554b616ed5
3rdit <llerdit@gmail.com>
2024-05-02 17:24:29 +0100

    feat: added run.bat for convenience

---
+++ frontend/run.bat
@@ -0,0 +1,27 @@
+@echo off
+REM Change directory to the React project directory
+cd %~dp0
+
+REM Build the React project
+echo Building the React app...
+npm run build
+
+REM Check if build was successful
+if not exist build\index.html (
+    echo Build failed, index.html not found.
+    exit /b
+)
+
+REM Copy the build directory to the Flask static folder
+echo Copying build to the Flask app...
+xcopy /E /I /Y build ..\backend\build
+
+REM Navigate to the backend directory
+cd ..\backend
+
+REM Start the Flask server
+echo Starting the Flask server...
+python run.py
+
+REM Keep the window open to see the output from the Flask server
+pause
